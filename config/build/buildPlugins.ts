784636f3e79c2fd94d427ff9efd8b5383745597a
Yelnur <zhazityelnur@gmail.com>
2023-03-01 02:00:45 +0600

    added config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { BuildPaths } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const buildPlugins = (paths: BuildPaths): webpack.WebpackPluginInstance[] => {
+ return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
+        })
+    ]
+}
+
+export default buildPlugins;
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import buildLoaders from './buildLoaders';
+import buildPlugins from './buildPlugins';
+import buildResolvers from './buildResolvers';
+import { BuildOptions } from './types/config';
+import buildDevServer from './buildDevServer';
+
+
+const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+
+    const {mode, paths, isDev} = options;
+
+    return {
+        mode: mode,
+          entry: paths.entry,
+          module: {
+              rules: buildLoaders(options),
+            },
+            resolve: buildResolvers(),
+          output: {
+              filename: '[name].[contenthash].js',
+              path: paths.build,
+              clean: true
+          },
+          plugins: buildPlugins(paths),
+          devtool: isDev ? 'inline-source-map' : undefined,
+          devServer: isDev ? buildDevServer(options) : undefined
+      }
+}
+
+export default buildWebpackConfig;
