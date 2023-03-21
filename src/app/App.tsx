06e983710ac5841634e25b078743a6cb43c4cc3d
Yelnur <zhazityelnur@gmail.com>
2023-03-21 17:45:03 +0600

    removed navbar from App: 12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     
     return (
       <div className={`app ${theme}`}>
+        <Navbar />
         <button onClick={toggleTheme}>Toggle</button>
-        <Link to={'/'}>Main</Link>
-        <Link to={'/about'}>About</Link>
           <AppRouter />
       </div>
      
